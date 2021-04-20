11b6eea445690c846bfc72beb1f2c9dad0ef74ea
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-04-20 21:51:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,8 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
-  if greeting == "greeting".upcase
-    return "NO, NOT SINCE 1938"
+  elsif greeting == greeting.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
